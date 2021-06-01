6b973e9161e870be58e8418676749e8f7ef05ec3
TrentHodgins <>
2021-06-01 16:16:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('button').addEventListener('click', displayMessage)
+/*This line tells the program to not send the alert untill the button is clicked*/
+
+function displayMessage () {
+//This line is telling the function to display a message//
+  alert('Hello')
+//This line tells the message to say "Hello"//
+}
